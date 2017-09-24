80773b8d7a5b2342fd69e091716003bc56aa8e03
Filipe Fernandes <ocefpaf@gmail.com>
2017-09-24 13:44:44 -0300

    update to 7.2.1

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,32 +1,10 @@
-#!/bin/bash
+# #!/bin/bash
 
-set -e # Abort on error.
-
-export PING_SLEEP=30s
-export WORKDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export BUILD_OUTPUT=$WORKDIR/build.out
-
-touch $BUILD_OUTPUT
-
-dump_output() {
-   echo Tailing the last 500 lines of output:
-   tail -500 $BUILD_OUTPUT
-}
-error_handler() {
-  echo ERROR: An error was encountered with the build.
-  dump_output
-  exit 1
-}
-
-# If an error occurs, run our error handler to output a tail of the build.
-trap 'error_handler' ERR
-
-# Set up a repeating loop to send some output to Travis.
-bash -c "while true; do echo \$(date) - building ...; sleep $PING_SLEEP; done" &
-PING_LOOP_PID=$!
-
-## START BUILD
+# START BUILD
 export HOSTTYPE="x86_64-linux"
+
+cp $RECIPE_DIR/site_specific.mk .
+cp $RECIPE_DIR/external_functions-ef_utility-site_specific.mk.in external_functions/ef_utility/site_specific.mk
 
 if [ $(uname) == Darwin ]; then
     export CC=clang
@@ -48,9 +26,10 @@ export LD_X11="-L/usr/lib64 -lX11"
 # Set in conda_forge_build_setup to `${MAKEFLAGS}` and that breaks the build here.
 export MAKEFLAGS=""
 
-make >> $BUILD_OUTPUT 2>&1
+make
+
 # make run_tests  # Image mismatch issues.
-make install >> $BUILD_OUTPUT 2>&1
+make install
 
 # Activate pyferret env vars.
 ACTIVATE_DIR=$PREFIX/etc/conda/activate.d
@@ -62,11 +41,3 @@ cp $RECIPE_DIR/scripts/activate.sh $ACTIVATE_DIR/pyferret-activate.sh
 cp $RECIPE_DIR/scripts/deactivate.sh $DEACTIVATE_DIR/pyferret-deactivate.sh
 cp $RECIPE_DIR/scripts/pyferret $PREFIX/bin/pyferret
 ln -s $PREFIX/bin/pyferret $PREFIX/bin/ferret
-
-## END BUILD
-
-# The build finished without returning an error so dump a tail of the output.
-dump_output
-
-# Nicely terminate the ping output loop.
-kill $PING_LOOP_PID
