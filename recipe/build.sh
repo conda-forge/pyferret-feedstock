12ee023d35a1b77fdad8f38a7166cf6f7e62413c
Filipe Fernandes <ocefpaf@gmail.com>
2018-05-03 15:58:42 -0300

    remove Travis-CI log workaround

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,35 +1,10 @@
 #!/bin/bash
 
-set -e # Abort on error.
+set -e
 
-export PING_SLEEP=30s
-export WORKDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export BUILD_OUTPUT=$WORKDIR/build.out
-
-touch $BUILD_OUTPUT
-
-dump_output() {
-   echo Tailing the last 500 lines of output:
-   tail -500 $BUILD_OUTPUT
-}
-error_handler() {
-  echo ERROR: An error was encountered with the build.
-  dump_output
-  exit 1
-}
-
-# If an error occurs, run our error handler to output a tail of the build.
-trap 'error_handler' ERR
-
-# Set up a repeating loop to send some output to Travis.
-bash -c "while true; do echo \$(date) - building ...; sleep $PING_SLEEP; done" &
-PING_LOOP_PID=$!
-
-## START BUILD
-## blank site_specific.mk files - just define environment here
+# blank site_specific.mk files - just define environment here
 touch site_specific.mk
 touch external_functions/ef_utility/site_specific.mk
-
 export DIR_PREFIX="$SRC_DIR"
 export PYTHON_EXE="python$(PY_VER)"
 export PYTHONINCDIR=`$PYTHON -c "from __future__ import print_function ; import distutils.sysconfig; print(distutils.sysconfig.get_python_inc())"`
@@ -63,9 +38,9 @@ fi
 # Set in conda_forge_build_setup to `${MAKEFLAGS}` and that breaks the build here.
 export MAKEFLAGS=""
 
-make >> $BUILD_OUTPUT 2>&1
+make
 # make run_tests  # Image mismatch issues.
-make install >> $BUILD_OUTPUT 2>&1
+make install
 
 # Activate pyferret env vars.
 ACTIVATE_DIR=$PREFIX/etc/conda/activate.d
@@ -77,11 +52,3 @@ cp $RECIPE_DIR/scripts/activate.sh $ACTIVATE_DIR/pyferret-activate.sh
 cp $RECIPE_DIR/scripts/deactivate.sh $DEACTIVATE_DIR/pyferret-deactivate.sh
 cp $RECIPE_DIR/scripts/pyferret $PREFIX/bin/pyferret
 ln -s $PREFIX/bin/pyferret $PREFIX/bin/ferret
-
-## END BUILD
-
-# The build finished without returning an error so dump a tail of the output.
-dump_output
-
-# Nicely terminate the ping output loop.
-kill $PING_LOOP_PID
